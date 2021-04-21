960664385c13e8515cd2e3c8c68e81d43234deb3
iluxandro01 <68388374+iluxandro01@users.noreply.github.com>
2021-04-21 20:03:09 +0300

    Added following camera

---
+++ Roguelike Platformer/Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float smoothTime;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    private void LateUpdate()
+    {
+        var playerPosition = player.position; //cached
+        var cameraPosition = transform.position; //cached
+        Vector3 targetPosition = new Vector3(playerPosition.x, playerPosition.y, cameraPosition.z);
+        transform.position = Vector3.SmoothDamp(cameraPosition, targetPosition, ref _velocity, smoothTime);
+    }
+}
